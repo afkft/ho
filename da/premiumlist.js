40e248843ecfa5031f7707f3b9d7d079b05115d2
afkft <olattfgj@gmail.com>
2026-01-19 12:41:12 +0100

    Update premiumlist.js

--- da/premiumlist.js
+++ da/premiumlist.js
@@ -11,6 +11,7 @@ const premiumUsers = [
 7457769202,
 6976365864,
 8290211822,
+8307830690, // Exp Jan 26 2026
 7772506739, // two week premium exp Jan 25 2026
   // Add more IDs here
 //one week people
